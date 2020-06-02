7d43c72e2909f7f78acd46ead56f11898a0c0656
guihRovetta <guilherme020213@hotmail.com>
2020-06-02 13:52:33 -0300

    add CORS and default image to create point

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -49,7 +49,8 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image:
+        'https://images.unsplash.com/photo-1580913428735-bd3c269d6a82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
