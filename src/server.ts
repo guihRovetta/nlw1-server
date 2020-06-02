21a803d8ad12beae30c0bbbc773be50e3ee0bc00
guihRovetta <guilherme020213@hotmail.com>
2020-06-02 10:29:46 -0300

    create and configuration database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(express.json());
 
-  response.json(['Diego', 'Guilherme', 'Teste', 'Daniel', 'Teste 6']);
-});
+app.use(routes);
 
 app.listen(3333);
